0d23e87e905674ae7c0bce7d5c8d36e97ee6b836
Арина Андреевна <Arina.Sheshukova@urfu.me>
2023-11-08 17:49:54 +0500

    доработка 5 раздела: подключила табы к компоненту фильма

--- src/pages/film-screen/film-screen.tsx
+++ src/pages/film-screen/film-screen.tsx
@@ -5,6 +5,8 @@ import { Film } from '../../types/film';
 import Footer from '../../components/footer/footer';
 import Logo from '../../components/logo/logo';
 import { AppRoute } from '../../const';
+import Tabs from '../../components/tabs/tabs';
+import { reviewsData } from '../../mocks/reviews';
 
 export default function FilmScreen({filmCards}: FilmScreenProps): JSX.Element {
   const {id} = useParams();
@@ -84,45 +86,7 @@ export default function FilmScreen({filmCards}: FilmScreenProps): JSX.Element {
                 height={327}
               />
             </div>
-            <div className="film-card__desc">
-              <nav className="film-nav film-card__nav">
-                <ul className="film-nav__list">
-                  <li className="film-nav__item film-nav__item--active">
-                    <a href="#" className="film-nav__link">
-                            Overview
-                    </a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a href="#" className="film-nav__link">
-                            Details
-                    </a>
-                  </li>
-                  <li className="film-nav__item">
-                    <a href="#" className="film-nav__link">
-                            Reviews
-                    </a>
-                  </li>
-                </ul>
-              </nav>
-              <div className="film-rating">
-                <div className="film-rating__score">{currentFilm.rating}</div>
-                <p className="film-rating__meta">
-                  <span className="film-rating__level">Very good</span>
-                  <span className="film-rating__count">{currentFilm.scoresCount} ratings</span>
-                </p>
-              </div>
-              <div className="film-card__text">
-                <p>{currentFilm.description}</p>
-                <p className="film-card__director">
-                  <strong>Director: {currentFilm.director}</strong>
-                </p>
-                <p className="film-card__starring">
-                  <strong>
-                            Starring: {currentFilm.starring.join(', ')}
-                  </strong>
-                </p>
-              </div>
-            </div>
+            <Tabs film={currentFilm} reviews={reviewsData}/>
           </div>
         </div>
       </section>
