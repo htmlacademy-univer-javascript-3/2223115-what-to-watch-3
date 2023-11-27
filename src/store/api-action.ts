206cdd9b9b8a3340a020befbed2ff5688f156f15
Арина Андреевна <Arina.Sheshukova@urfu.me>
2023-11-27 22:11:28 +0500

    добавила ассинхронную проверку на наличие авторизации

--- src/store/api-action.ts
+++ src/store/api-action.ts
@@ -1,8 +1,8 @@
 import {AxiosInstance} from 'axios';
 import { State, AppDispatch } from '../types/state';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { loadFilms, setFilmsDataLoadingStatus } from './action';
-import { APIRoute } from '../const';
+import { loadFilms, requireAuthorization, setFilmsDataLoadingStatus } from './action';
+import { APIRoute, AuthorizationStatus } from '../const';
 import { Film } from '../types/film';
 
 export const fetchFilmsAction = createAsyncThunk<void, undefined, {
@@ -18,3 +18,20 @@ export const fetchFilmsAction = createAsyncThunk<void, undefined, {
     dispatch(loadFilms(data));
   }
 );
+
+export const checkAuthAction = createAsyncThunk<void, undefined, {
+  dispatch: AppDispatch;
+  state: State;
+  extra: AxiosInstance;
+}>(
+  'user/checkAuth',
+  async (_arg, {dispatch, extra: api}) => {
+    try {
+      await api.get(APIRoute.Login);
+      dispatch(requireAuthorization(AuthorizationStatus.Auth));
+    } catch {
+      dispatch(requireAuthorization(AuthorizationStatus.NoAuth));
+    }
+  }
+);
+
