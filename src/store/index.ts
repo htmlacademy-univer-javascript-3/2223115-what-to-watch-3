e7ded5b4cab52f7e84a928309f5a8eb57ab34372
Арина Андреевна <Arina.Sheshukova@urfu.me>
2023-12-11 23:45:41 +0500

    удалила редьюсер из store(приложение не работает)

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,12 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
-import { reducer } from './reducer';
 import { createApi } from '../services/api';
 import { redirect } from './middlewares/redirect';
 
 const api = createApi();
 
 export const store = configureStore({
-  reducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       thunk: {
