105172f3356ead37d43d46038fa6b90702e8f958
Арина Андреевна <Arina.Sheshukova@urfu.me>
2023-12-18 18:37:12 +0500

    внесла правки в процесс получения данных о пользователе

--- src/store/api-action.ts
+++ src/store/api-action.ts
@@ -113,14 +113,15 @@ export const changeFavoriteStatusAction = createAsyncThunk<void, {id: string; st
   }
 );
 
-export const checkAuthAction = createAsyncThunk<void, undefined, {
+export const checkAuthAction = createAsyncThunk<UserData, undefined, {
   dispatch: AppDispatch;
   state: State;
   extra: AxiosInstance;
 }>(
   'user/checkAuth',
   async (_arg, {extra: api}) => {
-    await api.get(APIRoute.Login);
+    const {data} = await api.get<UserData>(APIRoute.Login);
+    return data;
   }
 );
 
